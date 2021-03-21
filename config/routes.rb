c4e069775bb050baf475125ded140a6456c0900b
Nicolas Chardeau <ncparis@hotmail.fr>
2021-03-21 20:10:37 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get 'about', to: 'pages#about', as: :about
-  get 'contact', to: 'pages#contact', as: :contact
-  get 'exhibitions', to: 'exhibitions#index', as: :exhibitions
-  get 'users', to: 'users#show', as: :users
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :exhibitions
+  resources :users
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
